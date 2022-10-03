cd85637ee8839981bca34362bc2a5a3b9c69075d
sisco321 <sasefa9@gmail.com>
2022-10-03 14:36:48 +0300

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# Sisay Assefa <sasefa9@gmail.com>
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
